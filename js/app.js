399c95651105a19331add06757cf4fbd7c0696fe
emonShimoul <emonshimoul@gmail.com>
2023-03-17 22:46:07 +0600

    functionality of removing the last value

--- js/app.js
+++ js/app.js
@@ -1,45 +1,52 @@
-function getPin(){
-    const pin = Math.round(Math.random()*10000);
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
     const pinString = pin + '';
-    if(pinString.length == 4){
+    if (pinString.length == 4) {
         return pin;
     }
-    else{
+    else {
         // console.log('got 3 digit and calling again');
         return getPin();
     }
 }
 
-function generatePin(){
+function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
 
-document.getElementById('key-pad').addEventListener('click', function(event){
+document.getElementById('key-pad').addEventListener('click', function (event) {
     const number = event.target.innerText;
     const calcInput = document.getElementById('typed-numbers');
-    if(isNaN(number)){
-        if(number == 'C'){
+    const previousNumber = calcInput.value;
+    if (isNaN(number)) {
+        if (number == 'C') {
             calcInput.value = '';
         }
+        else if (number == "<") {
+            // console.log(previousNumber);
+            const digits = previousNumber.split('');
+            digits.pop();
+            remainingDigits = digits.join('');
+            calcInput.value = remainingDigits;
+        }
     }
-    else{
-        const previousNumber = calcInput.value;
+    else {
         const newNumber = previousNumber + number;
         calcInput.value = newNumber;
     }
 });
 
-function verifyPin(){
+function verifyPin() {
     const pin = document.getElementById('display-pin').value;
     const typedNumbers = document.getElementById('typed-numbers').value;
     const successMessage = document.getElementById('notify-success');
     const failError = document.getElementById('notify-fail');
-    if(pin == typedNumbers){
+    if (pin == typedNumbers) {
         successMessage.style.display = 'block';
         failError.style.display = 'none';
     }
-    else{
+    else {
         failError.style.display = 'block';
         successMessage.style.display = 'none';
     }
